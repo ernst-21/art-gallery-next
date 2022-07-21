0a0c21fdb47b2b2b9a46564abed808483a352772
Ernesto Rivera <riverag8504@gmail.com>
2022-07-21 16:26:44 -0400

    [feature] created user menu in drawer & fixed issues

---
+++ pages/auth/profile/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainLayout from '../../components/layouts/MainLayout';
+
+const ProfilePage = () => {
+  return <MainLayout>Profile Page</MainLayout>;
+};
+
+export default ProfilePage;
